50c22bf3f027852fd6002259b603673f717bd331
maria rodriguez <maria.rodriguez@go1.com>
2022-06-29 01:21:46 -0500

    header

--- components/header.js
+++ components/header.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import DarkModeToggle from "react-dark-mode-toggle";
+import Provider, { useDarkmodeContext } from 'react-use-dark-mode';
+import styles from '../styles/About.module.css';
 
-export function header({  }) {
-  
+export function Header({  }) {
+  const [isDarkMode, setIsDarkMode] = useState(() => false);
   return (
-    <h1></h1>
+    <div className={styles.menuContainer}>
+      <div className={styles.titleButtons}>
+        <h1>El blog de Miguel</h1>
+        <nav>
+          <ul className={styles.list}>
+            <li className={styles.navButton}><a href="#">Artículos</a></li>
+            <li className={styles.navButton}><a href="#">Sobre mí</a></li>
+          </ul>
+        </nav>
+      </div>
+      <DarkModeToggle
+        onChange={setIsDarkMode}
+        checked={isDarkMode}
+        size={80}
+      />
+    </div>
+   
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
 import Image from 'next/image'
 import { MetadataBlog } from "../components/metadata/meta-data-blog";
-import  {Footer}  from "../components/footer";
+import  { Footer }  from "../components/footer";
+import  { Header }  from "../components/header";
 import PropTypes from 'prop-types';
+import styles from '../styles/About.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Header/>
       <MetadataBlog/>
       <main>
-        <h1 >
+        <h1 className={styles.textFooter}>
           holi
         </h1>
        
       </main>
-    <Footer/>
+      <Footer/>
     </div>
   )
 }
